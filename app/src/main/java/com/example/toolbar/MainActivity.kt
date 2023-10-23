2d697dd135650feef999d9fa7121ac008d0d4248
androidstd <androidstd@college.local>
2023-10-23 10:46:04 +0300

    завершение первого драфта приложения

--- app/src/main/java/com/example/toolbar/MainActivity.kt
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -9,14 +9,17 @@ import android.os.Parcel
 import android.os.Parcelable
 import android.view.MenuItem
 import android.widget.TextView
+import com.example.toolbar.databinding.ActivityMainBinding
 
 class MainActivity() : AppCompatActivity(), Parcelable {
+    lateinit var binding: ActivityMainBinding
     constructor(parcel: Parcel) : this() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
@@ -41,14 +44,48 @@ class MainActivity() : AppCompatActivity(), Parcelable {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return super.onOptionsItemSelected(item)
-
-
-        if (item.itemId == android.R.id.home) {
-            val txtHelloWorld = findViewById<TextView>(R.id.txtView)
+        /*if (item.itemId == android.R.id.home) {
+            val txtHelloWorld = findViewById<TextView>(binding.textView1)
             txtHelloWorld.setBackgroundColor(Color.argb(255, 240, 120, 128))
 
             Handler(Looper.getMainLooper()).postDelayed({
                 finish()
             }, 3000)
+        }*/
+    }
+
+}
+
+fun gameRPSLS (binding: ActivityMainBinding){
+    val Pchoise = (1..5).random()
+    var Uchoise = 0
+    when (binding.root.id.toString()){
+        "RockButton" -> Uchoise = 1
+        "PaperButton" -> Uchoise = 2
+        "ScissorsButton" -> Uchoise = 3
+        "LizardButton" -> Uchoise = 4
+        "SpockButton" -> Uchoise = 5
+    }
+    when(Uchoise){
+        1 -> when(Pchoise){
+            4, 3 -> binding.textAnswer.text = "Победа!"
+            1, 5 -> binding.textAnswer.text = "Поражение"
         }
+        2 -> when(Pchoise){
+            1, 5 -> binding.textAnswer.text = "Победа!"
+            3, 4 -> binding.textAnswer.text = "Поражение"
+        }
+        3 -> when(Pchoise){
+            2, 4 -> binding.textAnswer.text = "Победа!"
+            1, 5 -> binding.textAnswer.text = "Поражение"
+        }
+        4 -> when(Pchoise){
+            2, 5 -> binding.textAnswer.text = "Победа!"
+            1, 3 -> binding.textAnswer.text = "Поражение"
+        }
+        5 -> when(Pchoise){
+            1, 3 -> binding.textAnswer.text = "Победа!"
+            2, 4 -> binding.textAnswer.text = "Поражение"
+        }
+    }
 }
